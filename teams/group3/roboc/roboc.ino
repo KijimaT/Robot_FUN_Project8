fd13679ec970f078ceecbf909d9fd102b45bbcba
b1018225 <b1018225@fun.ac.jp>
2020-11-26 02:01:01 +0900

    自動起動のテスト

--- teams/group3/roboc/roboc.ino
+++ teams/group3/roboc/roboc.ino
@@ -4,54 +4,75 @@
 
 int deg = 90;
 int rx, vo, arg, flag;
-Servo neck, rs;
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Servo neck;//首
+Servo rs;//右肩
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *legMotor = AFMS.getMotor(1);
 Adafruit_DCMotor *headMotor = AFMS.getMotor(2);
-//Adafruit_DCMotor *rotateMotor  = AFMS.getMotor(3);
+Adafruit_DCMotor *rotateMotor  = AFMS.getMotor(3);
 
-/*TODO*/
 void setup() {
-  //Arduinoに電源を供給し、ロボットの動作がスタートする
+  //ArduinoにRaspberry piから電源を供給し、ロボットの動作がスタートする
   Serial.begin(9600);
   //首
   neck.attach(8);
+  neck.write(90);
   //右肩
   rs.attach(9);
- 
-  /*** 電源を入れた時の動作 ***/
-  uint8_t i;
-  Serial.print("init");
-  /* TODO */
-  //可動範囲調整が必要
-  //右肩を左右に動かす
-  rs.write();
-  //目線が上を向いて水平にする
-  //勢いよく上を向く
-  headMotor->run(FORWARD);
-   for (i = 0; i < 220; i=i+10) {
-    headMotor->setSpeed(i);
-    delay(10);
-  }
-  for (i=200; i!=0; i=i-10) {
-    headMotor->setSpeed(i);  
-    delay(10);
-  }
-  //一定の速度で水平に
-  headMotor->run(BACKWARD);
-  headMotor->setSpeed(i);
-    delay(10);
-  }
-  //首を左右に動かす
-  neck.write();
-  //Raspberry Piにシリアル通信で表情を指示する
-  Serial.write();
-  myMotor->run(RELEASE);
-  delay(1000);
-
-  Serial.print("stand by");
+  rs.write(90);
 }
 
 void loop() {
  
+  uint8_t i;
+  
+  
+  if (Serial.available() > 0) {
+
+    // 受信したデータを読み込む
+    int data = Serial.read();
+
+    if (data == '0') {
+    /* TODO */
+    /*** 電源を入れた時の動作 ***/
+      Serial.print("init");
+    //可動範囲調整が必要
+    //右肩を左右に動かす
+      rs.write(0);
+      delay(1000);
+      rs.write(80);
+    //目線が上を向いて水平にする
+    //勢いよく上を向く
+      headMotor->run(FORWARD);
+      for (i = 0; i < 220; i = i + 10) {
+        headMotor->setSpeed(i);
+        delay(10);
+      }
+      for (i = 200; i != 0; i = i - 10) {
+        headMotor->setSpeed(i);
+        delay(10);
+      }
+      //一定の速度で水平に
+      headMotor->run(BACKWARD);
+      headMotor->setSpeed(10);
+      delay(10);
+
+        //首を左右に動かす
+      neck.write(0);
+      delay(1000);
+      neck.write(150);
+      delay(1000);
+      neck.write(90);
+      //Raspberry Piにシリアル通信で表情を指示する
+      //myMotor->run(RELEASE);
+      delay(1000);
+      Serial.print("stand by");
+      Serial.write(0);//起動完了後Raspberry Piに0を返してデフォルトに移る
+    }else if (data == '1') {
+      //servo2.write(0);
+      Serial.write(2);
+    }else if(data == '2'){
+
+    }
+  }
 }
--- teams/group3/system_kao/system_kao.pde
+++ teams/group3/system_kao/system_kao.pde
@@ -6,34 +6,48 @@ int random = 0;
 int scene = 0;
 int count = 0;
 int flag = 0;//遷移状況管理用変数
+int signal=0;
 
 void setup(){
  background(0); 
  //fullScreen();
  size(400,250);
- mu2();
- //port = new Serial(this, "/dev/cu.usbmodem14101",9600);
- //port.write(0);//コードが起動した時にArduinoに起動したという合図を出す。
+ //mu2();
+ port = new Serial(this, "/dev/ttyACM0",9600);
+ port.write(0);//コードが起動した時にArduinoに起動したという合図を出す。
 }
 
 void draw(){
- count++;
- if(port.read() == 0){//Arduinoからint型の0を受け取った時default1()を実行（デフォルト状態にする）
-   ok();
+ //count++;
+ signal =port.read();
+ if(signal == 0){//Arduinoからint型の0を受け取った時default1()を実行（デフォルト状態にする）
    count = 0;
-   if(count > 300){
+   ok();
+   while(count<300){
+     count++;
+   }
+   if(count == 300){
+     port.write(1);
      default1();
      flag = 1;
    }
- }
- if(flag == 2)//デフォルト状態でタッチを受けた時
- {
-   count = 0;
-   egao();
-   if(count > 300){
+ }else if(signal == 2) {
+   if(flag == 1){
      default1();
-   }
+   }else if(flag == 2){//デフォルト状態でタッチを受けた時
+     count = 0;
+     egao();
+     while(count<300){
+      count++;
+     }
+     if(count == 300){
+      port.write(1);
+      flag = 1;
+      default1();
+    }
+  }
  }
+ 
    
 }
 
