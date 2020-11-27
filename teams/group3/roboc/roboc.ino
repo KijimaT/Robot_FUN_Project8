b33e8f7359e9a8e41339d4026383dbf70572e9b8
b1018225 <b1018225@fun.ac.jp>
2020-11-27 20:26:15 +0900

    Arduino↔RaspberryPi通信整理(未テスト)

--- teams/group3/roboc/roboc.ino
+++ teams/group3/roboc/roboc.ino
@@ -6,7 +6,7 @@ int deg = 90;
 int rx, vo, arg, flag;
 int mInit = 0;
 int sInit = 0;
-int data=0;
+int data=0;//RaspberryPiからの信号用
 Servo neck;//首
 Servo rs;//右肩
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
@@ -21,68 +21,70 @@ void setup() {
 
 void loop() {
   uint8_t i;
-  // 受信したデータを読み込む
+  //受信したデータを読み込む
   data = Serial.read();
     
     /** 電源を入れた時の動作 **/
-    
-    /*** モータの初期動作　***/
-    if (mInit == 0) {
-      neck.attach(8);
-      neck.write(90);
-      //右肩
-      rs.attach(9);
-      rs.write(90);
-      //Serial.print("init");
-      //可動範囲調整が必要
-      //右肩を左右に動かす
-      rs.write(0);
-      delay(1000);
-      rs.write(80);
-      //目線が上を向いて水平にする
-      //勢いよく上を向く
-      headMotor->run(FORWARD);
-      for (i = 0; i < 220; i = i + 10) {
-        headMotor->setSpeed(i);
-        delay(10);
-      }
-      for (i = 200; i != 0; i = i - 10) {
-        headMotor->setSpeed(i);
-        delay(10);
-      }
-      //一定の速度で水平に
-      headMotor->run(BACKWARD);
-      headMotor->setSpeed(10);
-      delay(10);
-
-      //首を左右に動かす
-      neck.write(0);
-      delay(1000);
-      neck.write(150);
-      delay(1000);
-      neck.write(90);
-      //Raspberry Piにシリアル通信で表情を指示する
-      //myMotor->run(RELEASE);
-      delay(1000);
-      mInit = 1; //モータの初期動作完了
-    }
-    if (sInit == 0) {
-      //Serial.print("stand by");
-      Serial.write(100);//起動完了後Raspberry Piに100を返してデフォルト表情に移るように指示
-      if(data ==1){
-        sInit =1;
+    if(data==0){//RaspberryPiとの通信の確認前
+      /*** モータの初期動作　***/
+      if (mInit == 0) {
+        neck.attach(8);
+        neck.write(90);
+        //右肩
+        rs.attach(9);
+        rs.write(90);
+        //Serial.print("init");
+        //可動範囲調整が必要
+        //右肩を左右に動かす
+        rs.write(0);
+        delay(1000);
+        rs.write(80);
+        //目線が上を向いて水平にする
+        //勢いよく上を向く
+        headMotor->run(FORWARD);
+        for (i = 0; i < 220; i = i + 10) {
+          headMotor->setSpeed(i);
+          delay(10);
         }
-    }else{
-      Serial.write(0);
-    }
+        for (i = 200; i != 0; i = i - 10) {
+          headMotor->setSpeed(i);
+          delay(10);
+        }
+        //一定の速度で水平に
+        headMotor->run(BACKWARD);
+        headMotor->setSpeed(10);
+        delay(10);
 
-    
-    if (data == 1 ) {
+        //首を左右に動かす
+        neck.write(0);
+        delay(1000);
+        neck.write(150);
+        delay(1000);
+        neck.write(90);
+        //Raspberry Piにシリアル通信で表情を指示する
+        //myMotor->run(RELEASE);
+        delay(1000);
+        mInit = 1; //モータの初期動作完了
+      }else{
+        Serial.write(100);
+      }
+      
+    }else if(data==1){
+      if (sInit == 0) {
+        //Serial.print("stand by");
+        Serial.write(100);//初回の通信
+        //RaspberryPiから1を受け取ったら
+          sInit = 1;//初回の通信を記録
+      }else{
+        Serial.write(101);//デフォルトに行けるように101を送信
+      }
+      //
+    }else if (data == 2 ) {
       Serial.write("");
-    } else if (data == 2) {
-      //servo2.write(0);
-      Serial.write(2);
     } else if (data == 3) {
+      //servo2.write(0);
+      Serial.write(3);
+    } else if (data == 4) {
       
     }
   }
--- teams/group3/system_kao/system_kao.pde
+++ teams/group3/system_kao/system_kao.pde
@@ -3,10 +3,10 @@ import processing.serial.*;
 Serial port;
 
 int random = 0;
-int scene = 0;
-int count = 0;
-int flag = 0;//遷移状況管理用変数
-int signal=0;
+int scene = 0;//画面遷移切り替え用変数
+int count = 0;//時間管理用変数
+int flag = 0;//タッチ管理での状態遷移状況管理用変数
+int signal=0;//arduinoからの値100~
 
 void setup(){
  background(0); 
@@ -19,33 +19,45 @@ void setup(){
 void draw(){
  signal =port.read();
  println(signal);
- 
- if(signal == 100){//Arduinoからint型の0を受け取っている時default1()を実行（デフォルト状態にする）
+
+/*signalが0か100の時*/
+if(signal==0){//signal=0(初期値)
+  /*TODO*/
+  //Loading...など表示する？
+
+  port.write(1);//Arduinoに1を送る
+  
+}else if(signal == 100){//Arduinoから100を受け取ってる時okを実行（デフォルト状態にする）
    count = 0;
    ok();
-   while(count<300){
-     count++;
-   }
-   if(count == 300){
-     port.write(2);
-     default1();
-     flag = 1;
-   }
- }else if(signal == 101) {
+   /*TODO*/
+   //Arduinoへokを表示していることを伝える
+   port.write(1);
+   //flag = 1;
+   
+ } else if(signal == 101) {
+   /*TODO*/
+   //通信のタイミング?が分からないので(?)
+   //初回の通信を確認したら(101)、時間経過でデフォルトに移行するようにしたい
+   //flagをどう使うか？
+   //→
    if(flag == 1){
      default1();
-   }else if(flag == 2){//デフォルト状態でタッチを受けた時
+   } else if(flag == 2){//デフォルト状態でタッチを受けた時
      count = 0;
      egao();
      while(count<300){
       count++;
      }
      if(count == 300){
-      port.write(1);//今のところArduinoからsignalはずっと2が帰ってくる
+      port.write(1);
       //default1();
       flag = 1;
     }
   }
+ } else if(signal ==102){
+   /*TODO*/
+   //起動完了後の動作
  }
 }
 
@@ -59,5 +71,4 @@ count は時間管理用変数。
 
 ・モーターの動作と顔の表情の連携についてはそれぞれの顔関数(kao部分にある関数)にあるport.write(番号)の番号により、
 　Arduino側に表情が変わったことを伝える。
-
-*/
+ →100番代にしてみた
